4a6098e14ace539af81ce94cada85c062cc240e1
Yamin1234 <yamin213811@example.com>
2021-12-18 19:48:00 +0630

    Updated status for enum

--- db/schema.rb
+++ db/schema.rb
@@ -1,16 +1,6 @@
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
-# be faster and is potentially less error prone than running all of your
-# migrations from scratch. Old migrations may fail to apply correctly if those
-# migrations use external dependencies or application code.
-#
-# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_17_075531) do
+
+ActiveRecord::Schema.define(version: 2021_12_17_093558) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
@@ -18,7 +8,8 @@ ActiveRecord::Schema.define(version: 2021_12_17_075531) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
-    t.index ["slug",name: "index_blogs_on_slug", unique:true, using: :btree]
+    t.integer "status", default: 0
+    t.index ["slug"], name: "index_blogs_on_slug", unique: true, using: :btree
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
